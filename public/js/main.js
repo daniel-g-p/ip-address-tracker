641a5154f8fe80462eaac3bf385ac665dedc81af
Daniel Giustini Perez <danielgiustiniperez@gmail.com>
2021-07-07 13:16:36 +0200

    integrate location api

--- public/js/main.js
+++ public/js/main.js
@@ -1,51 +1,94 @@
 const searchForm = document.querySelector(".search-form");
 const inputGroup = document.querySelector(".input-group");
 const searchQuery = document.querySelector(".input-group__input");
+const inputError = document.querySelector(".input-group__error");
 const searchOutputs = document.querySelectorAll(".result__output");
+const submitButton = document.querySelector(".input-group__button");
+const submitButtonArrow = document.querySelector(".input-group__button-arrow");
+const submitButtonLoader = document.querySelector(".loader");
+const ownIpButton = document.querySelector(".ownIP");
+
 const ipFormat = /^((25[0-5]|(2[0-4]|1[0-9]|[1-9]|)[0-9])(\.(?!$)|$)){4}$/;
-const domainFormat = /(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+[a-z0-9][a-z0-9-]{0,61}[a-z0-9]$/;
+const domainFormat = /([a-z0-9]+\.)?([a-z0-9][a-z0-9-]*)?((\.[a-z]{2,6})|(\.[a-z]{2,6})(\.[a-z]{2,6}))$/;
 
 searchForm.addEventListener("submit", async(e) => {
     e.preventDefault();
+    const inputType = validateQuery();
     if (!validateQuery()) {
-        triggerError(false);
+        triggerError(true);
         return;
     }
-    const result = await searchIpAddress(searchQuery.value);
-    console.log("Done");
+    const result = await fetchData(searchQuery.value, inputType);
+    console.log(result);
+    applyData(result);
+});
+
+ownIpButton.addEventListener("click", async() => {
+    const result = await fetchData("", "");
+    searchQuery.value = result.ip;
+    applyData(result);;
 });
 
 searchQuery.addEventListener("input", () => {
-    triggerError(true);
+    triggerError(false);
 })
 
-const searchIpAddress = async(query) => {
-    console.log(`Searching for ${query}`);
-    fetch(`https://geo.ipify.org/api/v1?apiKey=at_Qigj51U19P8DhOGJm7CWvhBPHCibw&ipAddress=${query}`)
+const fetchData = async(query, type) => {
+    toggleLoader(true);
+    return await fetch(`https://geo.ipify.org/api/v1?apiKey=at_Qigj51U19P8DhOGJm7CWvhBPHCibw&${type}=${query}`)
         .then(response => {
-            console.log(response.json());
+            toggleLoader(false);
             return response.json();
-        })
-        .catch(error => {
-            return "Error";
         });
 };
 
+const applyData = (data) => {
+    let requiredData;
+    try {
+        requiredData = [
+            data.ip,
+            `${data.location.city} ${data.location.postalCode}, ${data.location.region}, ${data.location.country}`,
+            `UTC ${data.location.timezone}`,
+            data.isp
+        ]
+    } catch {
+        requiredData = ["-", "-", "-", "-", ];
+        inputError.innerHTML = "Couldn't retrieve data for your search query...";
+        triggerError(true);
+    }
+    searchOutputs.forEach((element, index) => {
+        element.innerHTML = requiredData[index];
+    });
+}
+
 const validateQuery = () => {
-    q = searchQuery;
-    if (!q.value) {
-        return false;
-    } else if (!q.value.match(ipFormat) && !q.value.match(domainFormat)) {
-        return false;
+    q = searchQuery.value;
+    if (q.match(ipFormat)) {
+        return "ipAddress";
+    } else if (q.match(domainFormat)) {
+        return "domain";
     } else {
-        return true;
+        return false;
     }
 };
 
-const triggerError = (valid) => {
-    if (valid) {
-        inputGroup.classList.remove("input-group--error");
-    } else {
+const triggerError = (trigger) => {
+    if (trigger) {
         inputGroup.classList.add("input-group--error");
+    } else {
+        inputGroup.classList.remove("input-group--error");
+        setTimeout(() => {
+            inputError.innerHTML = "Please enter a valid IP address or domain..."
+        }, 250);
+    }
+}
+
+const toggleLoader = (toggle) => {
+    if (toggle) {
+        submitButtonArrow.classList.add("input-group__button-arrow--loading");
+        submitButtonLoader.classList.add("loader--loading");
+    } else {
+        submitButtonArrow.classList.remove("input-group__button-arrow--loading");
+        submitButtonLoader.classList.remove("loader--loading");
     }
 }
